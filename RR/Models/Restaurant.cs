d557f7777990cb48d167e568192e5001edeaae62
Greg Gonzalez <gregery.gonzalez@gmail.com>
2021-07-13 19:11:19 -0400

    Connection String added with class in Model

---
+++ RR/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RR.Models
+{
+    public class Restaurant//Plant
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants {get; set;}
+    }
+}
